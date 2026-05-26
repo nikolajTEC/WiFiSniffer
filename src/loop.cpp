35ec9db477434a5583e0c72fec802c44d1d78e4c
Loxtech <119638420+Loxtech@users.noreply.github.com>
2026-05-26 12:58:26 +0200

    Remove channel hopping logic from loop function

--- src/loop.cpp
+++ src/loop.cpp
@@ -28,10 +28,4 @@ void loop() {
     } else {
         maintainEspNowPeer();
     }
-
-    if (millis() - lastHopTime >= HOP_INTERVAL_MS) {
-        currentChannelIndex = (currentChannelIndex + 1) % CHANNEL_COUNT;
-        esp_wifi_set_channel(CHANNELS[currentChannelIndex], WIFI_SECOND_CHAN_NONE);
-        lastHopTime = millis();
-    }
 }
